fd99e32485507e322aed5b9330f9497a17c80720
xun <2042204285@qq.com>
2023-04-22 09:25:41 +0800

    moemnt

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import AppDataSource from '../ormConfig';
-import { Users } from './users/users.entity';
-
-AppDataSource.initialize()
-  .then(async () => {
-    const users = await AppDataSource.manager.find(Users);
-  })
-  .catch((error) => console.log(error));
